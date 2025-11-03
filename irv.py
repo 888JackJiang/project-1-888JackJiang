695997a312912cd63e646f6451d5a446a1d43b0e
888JackJiang <2293513117@qq.com>
2025-11-03 15:35:52 +0000

    add the first version of irv method to do transfers

--- irv.py
+++ irv.py
@@ -58,3 +58,112 @@ def update_ballots(ballots, to_eliminate):
 
 
 
+
+
+
+
+
+###### undo
+
+
+
+
+def vote_transfers(previous_ballots, updated_ballots):
+    """
+
+    Compare the changes in the first preference choices in the two ballot sets (before and after the elimination operation), 
+    and count how many ballots were discarded.
+    
+    core method: first we do a vectorization difference of the original ballot votes for fist preference and after using IRV method ones
+                second we do a zeros elements checking for the array rows one by one to search the discarded ballots in two ballots: previous_ballots and updated_ballots.
+                which is based on a looping.
+    Input:
+    ----------
+    previous_ballots : 
+        (np.ndarray)
+        Ballot array before a candidate elimination.
+        each row represents one ballot submitted by one voter;
+        each non-zero value represents a candidate;
+        each column represents a preference rank, from first to last preference going left to right, 
+        for example a 2 in the first column means that Candidate 2 was the voter's first preference;
+    updated_ballots : 
+        (np.ndarray)
+        Ballot array after a candidate elimination from using the function update_ballots()).
+        There we use IRV method to get the updated_ballots based on previous_ballots
+        
+    Outputs:
+    -------
+    candidates :
+        (np.ndarray)
+        candidates, a NumPy vector or list containing all candidates in the election in numerical order (e.g. [1 2 3 ...])  
+    vote_diffs : 
+        (np.ndarray)
+        a NumPy vector or list of the same length as candidates, containing the differences in first-preference votes 
+        obtained by each of the candidates between previous_ballots and updated_ballots (given in the same order as candidates)
+    
+        The difference in the number of first-choice votes between the updated ballot and the previous one. 
+        A positive value indicates an increase in the number of votes; a negative value indicates an increase in the number of lost votes.   
+        
+    discarded_diff : 
+        (int)
+        Number of ballots that became invalid/discarded (all zeros) after elimination.
+
+
+    Example
+    -------
+    we use the prev_ballots:
+    >>> vote_transfers(np.array([[1, 4, 0, 0],[1, 5, 3, 2, 4],[3, 5, 4, 1, 0]]),
+    and the updated_ballots:                np.array([[1, 4, 0, 0],[1, 5, 2, 4, 0],[5 ,4 ,1 ,0 , 0]]))
+    
+    we get the result like that: (candidates <- array([1, 2, 3, 4, 5]), vote_diffs <- array([0, 0, -1, 0, 1)
+    """
+    
+    # Count the total votes for the ballots program; which is the the number of rows of the array ballots.
+    n_votes = previous_ballots.shape[0]
+    
+    # Count first-preference we count the votes before and after voting based the corresponding oder candidate (default numerical order)
+    prev_cands, prev_counts = bd.count_votes(previous_ballots, preference=0, sort_by='candidate')
+    upd_cands, upd_counts   = bd.count_votes(updated_ballots, preference=0, sort_by='candidate')
+    
+    # we know prev_cands and upd_cands are the same results for candidates so we create a numpy vector or a list to store the candidates.
+    candidates = upd_cands
+
+    # Next we aim to align counts with candidate order
+    # Instead of one by one difference for two vectors we just use vectorization to do a difference for two vectors.
+    
+    
+    # # avoid the type is different before difference ; there we both use a Numpy vector
+    upd_counts = np.array(upd_counts)
+    prev_counts = np.array(prev_counts)
+    # we create the vote_ diffs: after - before.
+    vote_diffs = upd_counts - prev_counts
+    
+
+    # Next we start counting discarded ballots
+    
+    # Initialise
+    discarded_prev = 0 # we start the loop which we want to do a accumulation for the the number    
+    # we start to loop the row of the matrix from the number of the n_votes which is equal to the number of the row of the matrix.
+    for i in range(n_votes): 
+        # we check whether or not the row i all the elements are zero 
+        # the condition is that when row i each element is 0 then the sum of all the elements are zero, if there exists no zero item then the sum of the row isn't equal to zero
+        if np.sum(previous_ballots[i,:])==0 :
+            discarded_pre += 1 # update the value find the row is 0 (discarded) plus1
+            
+    # avoid the type is different before difference ; there we both use a Numpy vector
+    discarded_upd = np.array(discarded_upd)
+    discarded_prev = np.array(discarded_prev)       
+    
+    # get the total situation before and after
+    discarded_diff = discarded_upd - discarded_prev
+
+    return candidates, vote_diffs, discarded_diff
+
+
+
+
+
+
+
+
+
